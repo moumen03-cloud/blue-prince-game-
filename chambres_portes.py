e5785efffe444144e9c5cb1fa94daf33564bb1e2
moumen03-cloud <moumenfcb03@gmail.com>
2025-11-08 15:54:43 +0100

    portes

---
+++ chambres_portes.py
@@ -0,0 +1,41 @@
+DEFAULT_EXITS = { 
+   
+    # Pièces bleues 
+    "AQUARIUM": {"haut": 1, "bas": 0, "gauche": 1, "droite": 1},
+    "DEN": {"haut": 1, "bas": 1, "gauche": 1, "droite": 0},
+    "WEST WING HALL": {"haut": 0, "bas": 0, "gauche": 1, "droite": 0}, 
+    "DRAWING ROOM": {"haut": 1, "bas": 1, "gauche": 1, "droite": 0},
+    "SOLARIUM": {"haut": 1, "bas": 0, "gauche": 0, "droite": 0},
+    "CONFERENCE ROOM": {"haut": 1, "bas": 1, "gauche": 1, "droite": 0},
+    "NOOK": {"haut": 0, "bas": 0, "gauche": 1, "droite": 0},
+    "PARLOR": {"haut": 0, "bas": 1, "gauche": 1, "droite": 0},
+    "THE POOL": {"haut": 1, "bas": 1, "gauche": 1, "droite": 0}, 
+    "RUMPUS ROOM": {"haut": 1, "bas": 1, "gauche": 0, "droite": 0},
+    "SPARE ROOM": {"haut": 1, "bas": 1, "gauche": 0, "droite": 0},
+    "STUDY": {"haut": 0, "bas": 0, "gauche": 0, "droite": 1},
+    "WALK-IN CLOSET": {"haut": 0, "bas": 0, "gauche": 0, "droite": 1},
+    "WORKSHOP": {"haut": 1, "bas": 1, "gauche": 0, "droite": 0},
+
+    # Pièces marrons 
+    "HALLWAY": {"haut": 1, "bas": 1, "gauche": 0, "droite": 1}, # Couloir vertical
+    "CLOSET": {"haut": 0, "bas": 0, "gauche": 1, "droite": 0},
+    "STOREROOM": {"haut": 0, "bas": 1, "gauche": 0, "droite": 0},
+    "CORRIDOR": {"haut": 0, "bas": 0, "gauche": 1, "droite": 1}, # Couloir horizontal
+    "BEDROOM": {"haut": 1, "bas": 0, "gauche": 0, "droite": 1},
+    "CLOISTER": {"haut": 1, "bas": 1, "gauche": 1, "droite": 1},
+    "COMMISSARY": {"haut": 0, "bas": 1, "gauche": 0, "droite": 1},
+    "DINING ROOM": {"haut": 1, "bas": 1, "gauche": 0, "droite": 1},
+    "FURNACE": {"haut": 0, "bas": 1, "gauche": 0, "droite": 0},
+    "GUEST BEDROOM": {"haut": 0, "bas": 1, "gauche": 0, "droite": 0},
+    "GYMNASIUM": {"haut": 1, "bas": 1, "gauche": 0, "droite": 1},
+    "KITCHEN": {"haut": 1, "bas": 0, "gauche": 0, "droite": 1},
+    "LAVATORY": {"haut": 0, "bas": 0, "gauche": 1, "droite": 0},
+    "PANTRY": {"haut": 1, "bas": 0, "gauche": 0, "droite": 1},
+    "PATIO": {"haut": 1, "bas": 0, "gauche": 1, "droite": 0},
+    "SERVANT'S QUARTERS": {"haut": 0, "bas": 0, "gauche": 1, "droite": 0},
+     "BOUDOIR": {"haut": 0, "bas": 1, "gauche": 1, "droite": 0},
+    # Pièces spéciales (Point de départ / Objectif)
+    "ENTRANCE HALL": {"haut": 1, "bas": 0, "gauche": 1, "droite": 1}, 
+    "ANTECHAMBER": {"haut": 0, "bas": 1, "gauche": 0, "droite": 0}, 
+   
+    }
