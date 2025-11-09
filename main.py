d159e24cc26720b757af4e709693dc8b71e91d76
sahouanerami-pixel <sahouanerami@gmail.com>
2025-11-09 01:11:41 +0100

    ajout de parametres frais d’entrée et resources a la methode de class romm

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-
 class Room:
-    def __init__(self, x, y, room_type="standard",exits=None, name="ROOM", ):
+    def __init__(self, x, y, room_type="standard", resources=None, exits=None, name="ROOM", entrance_fee=0):
         self.pos_x = x
         self.pos_y = y
         self.room_type = room_type
-        self.cost = 1 if room_type == "special" else 0  
-        
+        self.cost = 1 if room_type == "special" else 0  # coût de pose (hérité)
+        # [TOOLS] on ajoute "tools" (liste) dans les ressources
+        self.resources = resources if resources is not None else {
+            "bread": 0, "coins": 0, "gems": 0, "keys": 0, "dice": 0, "tools": []
+        }
         self.exits = exits if exits is not None else {}
         self.visited = False
         self.name = name
+        # [PAID] frais d’entrée (coins) à payer pour accéder/collecter
+        self.entrance_fee = max(0, int(entrance_fee))
        
 class Player:
     def __init__(self, start_steps: int):
