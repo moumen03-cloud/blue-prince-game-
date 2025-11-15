3261bdd9ae64c20871bcb37814cb0d01c1ddd268
moumen03-cloud <moumenfcb03@gmail.com>
2025-11-15 13:04:56 +0100

    Organisation du dossier chambres

--- chambres_portes.py
+++ chambres_portes.py
@@ -1,3 +1,5 @@
+import os
+
 DEFAULT_EXITS = { 
    
     # Pièces bleues 
@@ -39,3 +41,43 @@ DEFAULT_EXITS = {
     "ANTECHAMBER": {"haut": 0, "bas": 1, "gauche": 0, "droite": 0}, 
    
     }
+
+
+IMAGE_FOLDER = "images projet"
+BACKGROUND_IMAGE_PATH = os.path.join(IMAGE_FOLDER, "image arriere plan.png")
+
+ROOM_IMAGES = {
+    "ANTECHAMBER": os.path.join(IMAGE_FOLDER, "antechamber.png.png"),
+    "AQUARIUM": os.path.join(IMAGE_FOLDER, "aquarium.png.png"),
+    "BEDROOM": os.path.join(IMAGE_FOLDER, "bedroom.png.png"),
+    "BOUDOIR": os.path.join(IMAGE_FOLDER, "boudoir.png"),
+    "CLOISTER": os.path.join(IMAGE_FOLDER, "cloister.png.png"),
+    "CLOSET": os.path.join(IMAGE_FOLDER, "closet.ong.png"),
+    "COMMISSARY": os.path.join(IMAGE_FOLDER, "commissary.png.png"),
+    "CONFERENCE ROOM": os.path.join(IMAGE_FOLDER, "conference room.png.png"),
+    "CORRIDOR": os.path.join(IMAGE_FOLDER, "corridor.png.png"),
+    "DEN": os.path.join(IMAGE_FOLDER, "den.png.png"),
+    "DINING ROOM": os.path.join(IMAGE_FOLDER, "dining room.png.png"),
+    "DRAWING ROOM": os.path.join(IMAGE_FOLDER, "drawing room.png.png"),
+    "ENTRANCE HALL": os.path.join(IMAGE_FOLDER, "entrance hall.png.png"),
+    "FURNACE": os.path.join(IMAGE_FOLDER, "furnace.png.png"),
+    "GUEST BEDROOM": os.path.join(IMAGE_FOLDER, "guest room.png.png"),
+    "GYMNASIUM": os.path.join(IMAGE_FOLDER, "gymnasium.png.png"),
+    "HALLWAY": os.path.join(IMAGE_FOLDER, "hallway.png.png"),
+    "KITCHEN": os.path.join(IMAGE_FOLDER, "kitchen.png.png"),
+    "LAVATORY": os.path.join(IMAGE_FOLDER, "lvatory.png.png"),
+    "NOOK": os.path.join(IMAGE_FOLDER, "nook.png.png"),
+    "PANTRY": os.path.join(IMAGE_FOLDER, "pantry.png.png"),
+    "PARLOR": os.path.join(IMAGE_FOLDER, "parlor.png.png"),
+    "PATIO": os.path.join(IMAGE_FOLDER, "patio.png.png"),
+    "THE POOL": os.path.join(IMAGE_FOLDER, "pool.png.png"),
+    "RUMPUS ROOM": os.path.join(IMAGE_FOLDER, "rumpus room.png"),
+    "SERVANT'S QUARTERS": os.path.join(IMAGE_FOLDER, "sevant's quarters.png.png"),
+    "SOLARIUM": os.path.join(IMAGE_FOLDER, "solarium.png.png"),
+    "SPARE ROOM": os.path.join(IMAGE_FOLDER, "spare room.png.png"),
+    "STOREROOM": os.path.join(IMAGE_FOLDER, "storeroom.png.png"),
+    "STUDY": os.path.join(IMAGE_FOLDER, "study.png.png"),
+    "WALK-IN CLOSET": os.path.join(IMAGE_FOLDER, "walk in closet.png.png"),
+    "WEST WING HALL": os.path.join(IMAGE_FOLDER, "west wing hall.png.png"),
+    "WORKSHOP": os.path.join(IMAGE_FOLDER, "worksop.png.png"),
+}
